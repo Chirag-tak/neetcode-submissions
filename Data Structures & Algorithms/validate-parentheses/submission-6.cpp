c5508b0fde3967d5632139b20f4c2673bb719a28
Chirag Tak <112536182+Chirag-tak@users.noreply.github.com>
2026-06-16 10:30:29 +0530

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> str;
+        bool flag=0;
+        if (s.length()<=1){
+            return 0;
+        }
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='{' || s[i]=='['|| s[i]=='('){
+                str.push(s[i]);
+            }
+            if(str.empty()){return 0;}
+            if(s[i]=='}' || s[i]==']'|| s[i]==')'){
+                 if((abs(s[i]-str.top())==1)||(abs(s[i]-str.top())==2)){
+                    str.pop();
+                 }else{
+                    flag=1;
+                 }
+            }
+        }
+        if(str.empty() && flag==0){return 1;}
+        return 0;
+    }
+};
