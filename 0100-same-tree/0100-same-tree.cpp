9e36a298fbca8175583806ba256aa3e07b064c1e
Chirag Tak <112536182+Chirag-tak@users.noreply.github.com>
2026-07-27 12:32:18 +0530

    Time: 0 ms (100%), Space: 12.7 MB (78.49%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -1,37 +1,17 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
-    bool treverse(TreeNode *p,TreeNode *q){
-         if(p && !q){
-            return false;
-        }
-        if(q && !p){
-            return false;
-        }
-        if(!p && !q){
-            return true;
-        }
-        while(p && q){
-            if(p->val==q->val){
-                return (treverse(p->right,q->right) && treverse(p->left,q->left));
-            }else{
-                return false;
-            }
-        }
-        return false;
-    }
-public:
+    bool treverse(TreeNode* p, TreeNode* q) {
+        if (!p && !q) return true;
+        if (!p || !q) return false;
 
+        if (p->val != q->val)
+            return false;
+
+        return treverse(p->left, q->left) &&
+               treverse(p->right, q->right);
+    }
+
+public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-    return treverse(p,q);
+        return treverse(p, q);
     }
 };
